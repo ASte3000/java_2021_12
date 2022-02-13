3034140a215a094d6b773083b83286d9394c02e8
ASte3000 <84619332+ASte3000@users.noreply.github.com>
2022-02-13 18:02:29 +0300

    hw04-collections implementation

--- hw04-collections/src/main/java/homework/CustomerReverseOrder.java
+++ hw04-collections/src/main/java/homework/CustomerReverseOrder.java
@@ -1,16 +1,17 @@
 package homework;
 
 
-public class CustomerReverseOrder {
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-    //todo: 2. надо реализовать методы этого класса
-    //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
+public class CustomerReverseOrder {
+    private final Deque<Customer> deque = new ArrayDeque<>();
 
     public void add(Customer customer) {
-
+        deque.add(customer);
     }
 
     public Customer take() {
-        return null; // это "заглушка, чтобы скомилировать"
+        return deque.pollLast();
     }
 }
--- hw04-collections/src/main/java/homework/CustomerService.java
+++ hw04-collections/src/main/java/homework/CustomerService.java
@@ -1,23 +1,30 @@
 package homework;
 
 
-import java.util.Map;
+import java.util.*;
 
 public class CustomerService {
-
-    //todo: 3. надо реализовать методы этого класса
-    //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+    private final TreeMap<Customer, String> map = new TreeMap<>(Comparator.comparingLong(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
-        //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return null; // это "заглушка, чтобы скомилировать"
+        return createDeepCopy(map.firstEntry());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return null; // это "заглушка, чтобы скомилировать"
+        return createDeepCopy(map.higherEntry(customer));
     }
 
     public void add(Customer customer, String data) {
+        map.put(customer, data);
+    }
 
+    private static Map.Entry<Customer, String> createDeepCopy(Map.Entry<Customer, String> entry) {
+        if (entry == null)
+            return null;
+
+        Customer customer = entry.getKey();
+        Customer customerCopy = new Customer(customer.getId(), customer.getName(), customer.getScores());
+
+        return new AbstractMap.SimpleImmutableEntry<>(customerCopy, entry.getValue());
     }
 }
