aeabe888b92696cee2200da5d0df847a9b809e1c
ASte3000 <84619332+ASte3000@users.noreply.github.com>
2022-03-08 23:18:02 +0300

    hw08-gc: performance optimization

--- hw08-gc/src/main/java/ru/calculator/CalcDemo.java
+++ hw08-gc/src/main/java/ru/calculator/CalcDemo.java
@@ -15,8 +15,8 @@ import java.time.LocalDateTime;
 
 public class CalcDemo {
     public static void main(String[] args) {
-        long counter = 100_000_000;
-        var summator = new Summator();
+        var counter = 100_000_000;
+        var summator = new Summator(counter);
         long startTime = System.currentTimeMillis();
 
         for (var idx = 0; idx < counter; idx++) {
